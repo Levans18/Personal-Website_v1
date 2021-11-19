95dc9240f891969352900f6cba3d08daceaed6ab
Liam Evans <liamevans1818@gmail.com>
2021-11-19 15:52:24 -0500

    some animations

--- JS/contact.js
+++ JS/contact.js
@@ -1,28 +1,36 @@
 
 
+
 let animateContact = () => {
-    setFigureStart();
-    middleTranslate();
-    setTimeout(() => {outlierTranslate()},1000)
+    noFigcaption();
+    setTimeout(() => {middleTranslate()},200);
+    setTimeout(() => {outlierTranslate()},1000);
+}
+
+let noFigcaption = () => {
+    anime({
+        targets: "contact-widgets figure figcaption",
+        scale: 0,
+    })
 }
 
 let middleTranslate = () => {
     anime({
         targets: '.contact-widgets figure',
-        translateX: screen.width/3 + 100,
-        duration: 2000
+        translateX: screen.width/3 + 300,
+        duration: 2000,
     });  
 }
 
 let outlierTranslate = () => {
     anime({
         targets: '.email',
-        translateX: 100,
-        duration: 2000
+        translateX: 300,
+        duration: 2000,
     });
     anime({
         targets: '.github',
-        translateX: screen.width/1.40,
-        duration: 2000
+        translateX: screen.width/1.40 + 200,
+        duration: 2000,
     });
 }
