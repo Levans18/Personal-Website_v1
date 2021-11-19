40130553c469b3354a73fb1185e1953270138a94
Liam Evans <liamevans1818@gmail.com>
2021-11-19 15:41:20 -0500

    i fucked it

--- JS/contact.js
+++ JS/contact.js
@@ -1,8 +1,28 @@
-anime({
-    targets: '.email img',
-    translateX: 100,
-    borderRadius: 50,
-    duration: 2000,
-    easing: 'linear',
-    direction: 'alternate'
-});    
+
+
+let animateContact = () => {
+    setFigureStart();
+    middleTranslate();
+    setTimeout(() => {outlierTranslate()},1000)
+}
+
+let middleTranslate = () => {
+    anime({
+        targets: '.contact-widgets figure',
+        translateX: screen.width/3 + 100,
+        duration: 2000
+    });  
+}
+
+let outlierTranslate = () => {
+    anime({
+        targets: '.email',
+        translateX: 100,
+        duration: 2000
+    });
+    anime({
+        targets: '.github',
+        translateX: screen.width/1.40,
+        duration: 2000
+    });
+}
